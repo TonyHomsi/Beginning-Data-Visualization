1c77b267f12294cc9091be433f0b4a9dfb4f1ea0
Tony Homsi <tonyhomsi@hotmail.com>
2018-01-13 11:49:59 +0100

    Done!

--- Visualizing Two Categorical Variables(Qualitative Bivariate).R
+++ Visualizing Two Categorical Variables(Qualitative Bivariate).R
@@ -80,8 +80,11 @@ mosaicplot(
 
 
 ###### Using Lattice
-install.packages("barchart")
-library(barchart)
+install.packages("lattice")
+library(lattice)
+
+require(lattice)
+
 # Create a grouped frequency bar chart
 barchart(
   x = awards,
@@ -109,3 +112,63 @@ barchart(
     y = 0.95,
     title = "Awards",
     text = c("No", "Yes")))
+
+
+# Create a proportional frequency table
+matrix <- apply(awards, 1, function(x) {x / sum(x) })
+
+proportions <- t(matrix) # transpose matrix
+
+head(awards)
+
+head(proportions)
+
+# Create a 100% stacked frequency bar chart
+barchart(
+  x = proportions,
+  stack = TRUE,
+  horizontal = FALSE,
+  main = "Proportion of Movies by Rating and Award",
+  xlab = "Rating",
+  ylab = "Proportion of Movies",
+  auto.key = list(
+    x = 0.70,
+    y = 1.05,
+    title = "Awards",
+    columns = 2,
+    text = c("No", "Yes"),
+    background = "white"))
+
+# NOTE: No spine plot or mosaic plot in lattice
+
+
+####### Using ggplot2
+
+
+library(ggplot2)
+# Create a grouped frequency bar chart
+ggplot(
+  data = movies, 
+  aes(x = Rating, fill = Awards)) +
+  geom_bar(position = "dodge") +
+  ggtitle("Count of Movies by Rating and Awards") +
+  scale_fill_discrete(labels=c("No","Yes"))
+
+
+# Create a stacked frequency bar chart
+ggplot(
+  data = movies, 
+  aes(x = Rating, fill = Awards)) +
+  geom_bar() +
+  ggtitle("Count of Movies by Rating and Awards") +
+  scale_fill_discrete(labels = c("No", "Yes"))
+
+# Create a 100% stacked frequency bar chart
+ggplot(
+  data = movies, 
+  aes(x = Rating, fill = Awards)) +
+  geom_bar(position = "fill") +
+  ggtitle("Proportion of Movies by Rating and Awards") +
+  ylab("Proportion of Movies") +
+  scale_fill_discrete(labels = c("No", "Yes"))
+
